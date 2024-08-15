4a3914390b62a536ada3d7dbde7adb904b8ce957
AtulGoyala <165156690+AtulGoyala@users.noreply.github.com>
2024-08-15 11:00:43 +0530

    Add files via upload

---
+++ MyPersonalProjecttsixnsdt4rtwer-10/src/main/java/com/chat/app/MyPersonalProjecttsixnsdt4rtwer10Application.java
@@ -0,0 +1,15 @@
+package com.chat.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPersonalProjecttsixnsdt4rtwer10Application
+{
+
+	public static void main(String[] args) 
+	{
+		SpringApplication.run(MyPersonalProjecttsixnsdt4rtwer10Application.class, args);
+	}
+
+}
